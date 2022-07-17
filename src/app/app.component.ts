c0194a05cb4d5ee1c9ed3a272675542568d2bbf8
Anthony Rojas Calderon <anthonyrojas773@gmail.com>
2022-07-16 20:12:48 -0600

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'front-end';
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.peliculasEnCines = [
+        {
+        titulo: 'Spider-Man',
+        fechaLanzamiento: new Date(),
+        precio: 1400.99
+      },
+      {
+        titulo: 'Moana',
+        fechaLanzamiento: new Date('2016-11-14'),
+        precio: 400.99
+      }
+    ]
+    }, 3000);
+
+  }
+
+  peliculasEnCines;
+  peliculasProximosEstrenos = [
+  ];
+
 }
